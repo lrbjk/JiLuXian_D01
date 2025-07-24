e2f488fc52a058eda4c914a10e6afa126e08924e
lrbjk <118360194+lrbjk@users.noreply.github.com>
2025-07-24 17:08:03 +0800

    伤害接口
    DamageContext类里边儿写一些具体的伤害数值

--- Assets/Rainbow/Scripts/Interface/Interface.cs
+++ Assets/Rainbow/Scripts/Interface/Interface.cs
@@ -3,3 +3,11 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+public interface IDamage
+{
+   /// <summary>
+   /// ¹¥»÷·½·¨
+   /// </summary>
+   /// <param name="damageContext"></param>
+    public void TakeDamage(DamageContext damageContext);
+}
