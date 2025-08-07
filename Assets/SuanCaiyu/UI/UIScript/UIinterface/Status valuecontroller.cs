2f3332735b960f4fd7deb3e7475fb6e5ea290588
suancaiyu <3328160241@qq.com>
2025-08-07 19:28:58 +0800

    血条动效

--- Assets/SuanCaiyu/UI/UIScript/UIinterface/Status valuecontroller.cs
+++ Assets/SuanCaiyu/UI/UIScript/UIinterface/Status valuecontroller.cs
@@ -8,20 +8,28 @@ public class Statusvaluecontroller : MonoBehaviour
     [Header("Slider Reference")]
     [SerializeField] private Image statusValueimage;
     [SerializeField] private Image statusValueBack;
+    [SerializeField] private Image backGround;
 
     [SerializeField] private float statusValueTime;
-    
+
+    [SerializeField] private float maxValue;
+
+    public Ease easeType = Ease.OutQuad;
+
     /// <summary>
     /// 仅供测试
     /// </summary>
     [Header("按钮配置")]
     [SerializeField] private Button targetButton1;
     [SerializeField] private Button targetButton2;
+    [SerializeField] private Button targetButton3;
+    [SerializeField] private Button targetButton4;
 
     private void Start()
     {
-        statusValueimage.fillAmount = 1;
-        statusValueTime = 0.5f;
+        //初始化当前宽度
+        maxValue = statusValueimage.GetComponent<RectTransform>().rect.width;
+        statusValueTime = 0.4f;
 
 
         // 添加点击事件监听
@@ -29,6 +37,8 @@ public class Statusvaluecontroller : MonoBehaviour
         {
             targetButton1.onClick.AddListener(StartCoroutineOnClick_increse);
             targetButton2.onClick.AddListener(StartCoroutineOnClick_decrese);
+            targetButton3.onClick.AddListener(StartCoroutionOnClick_increseMax);
+            targetButton4.onClick.AddListener (StartCoroutionOnClick_decreseMax);
         }
         else
         {
@@ -38,56 +48,139 @@ public class Statusvaluecontroller : MonoBehaviour
     }
 
     /// <summary>
-    /// 点击测试
+    /// 外部直接调用的接口
     /// </summary>
     private void StartCoroutineOnClick_increse()
     {
-        StartCoroutine(Increase(0.1f));
+        StartCoroutine(Increase(100f));
     }
 
     private void StartCoroutineOnClick_decrese()
     {
-        StartCoroutine(Decrease(0.1f));
+        StartCoroutine(Decrease(100f));
+    }
+
+    private void StartCoroutionOnClick_increseMax()
+    {
+        StartCoroutine(IncreseMax(100f));
+    }
+
+    private void StartCoroutionOnClick_decreseMax()
+    {
+        StartCoroutine(DecreseMax(100f));
     }
 
 
 
-   
 
     /// <summary>
-    /// 直接增加状态值
+    /// 实血直接增加状态值
     /// </summary>
     /// <param name="amount"></param>
     public void IncreaseValue(float amount)
     {
-        float currentAmount = Mathf.Clamp(statusValueimage.fillAmount + amount, 0f, 1f);
+        RectTransform rectTransform = statusValueimage.GetComponent<RectTransform>();
 
-        DOTween.To(() => statusValueimage.fillAmount, x => statusValueimage.fillAmount = x, currentAmount, 0.2f)
-          .OnUpdate(() => Debug.Log($"回复当前值: {statusValueimage.fillAmount}"))
-          .OnComplete(() => Debug.Log("回复已达到目标值"));
+        float targetWidth = Mathf.Clamp(rectTransform.rect.width + amount, 0f, maxValue);
+
+        rectTransform.DOSizeDelta(new Vector2(targetWidth, rectTransform.sizeDelta.y), statusValueTime)
+           .SetEase(easeType);
+
     }
 
     /// <summary>
-    /// 直接减少状态值
+    /// 实血直接减少状态值
     /// </summary>
     /// <param name="amount"></param>
     public void DecreaseValue_im(float amount)
     {
-        statusValueimage.fillAmount = Mathf.Clamp(statusValueimage.fillAmount - amount, 0f, 1f);
+        RectTransform rectTransform = statusValueimage.GetComponent<RectTransform>();
+        Debug.Log("血量减少！");
+        float targetWidth = Mathf.Clamp(rectTransform.rect.width - amount, 0f, maxValue);
+
+        rectTransform.sizeDelta = new Vector2(targetWidth, rectTransform.sizeDelta.y);
     }
 
     /// <summary>
-    /// 逐步增加状态值
+    /// 虚血逐步改变变量值 
     /// </summary>
     /// <param name="amount"></param>
     /// <returns></returns>
     public void ChangeValue_step()
     {
-        //float currentValue = statusValueBack.fillAmount;
+        RectTransform rectTransform = statusValueimage.GetComponent<RectTransform>();
+        RectTransform rectTransform_1 = statusValueBack.GetComponent<RectTransform>();
 
-        DOTween.To(() => statusValueBack.fillAmount, x => statusValueBack.fillAmount = x, statusValueimage.fillAmount, statusValueTime)
-           .OnUpdate(() => Debug.Log($"当前值: {statusValueBack.fillAmount}"))
-           .OnComplete(() => Debug.Log("已达到目标值"));
+        float targetWidth = rectTransform.rect.width;
+        
+        rectTransform_1.DOSizeDelta(new Vector2(targetWidth, rectTransform_1.sizeDelta.y), statusValueTime)
+           .SetEase(easeType);
+
+
+    }
+
+    /// <summary>
+    /// 直接增加上限
+    /// </summary>
+    /// <param name="amount"></param>
+    public void IncreasemMax(float amount)
+    {
+        //更新最大上限
+        maxValue = maxValue + amount;
+        RectTransform rectTransform = backGround.GetComponent<RectTransform>();
+        
+        float targetWidth = Mathf.Clamp(maxValue + 10f , 0f, maxValue +  10f);
+
+        rectTransform.DOSizeDelta(new Vector2(targetWidth, rectTransform.sizeDelta.y), statusValueTime)
+           .SetEase(easeType);
+    }
+
+    /// <summary>
+    /// 上限变化时虚血实血逐步变化
+    /// </summary>
+    public void IncreseValueToMax(float amount)
+    {
+        RectTransform rectTransform = statusValueimage.GetComponent<RectTransform>();
+        RectTransform rectTransform_1 = statusValueBack.GetComponent<RectTransform>();
+
+        float targetWidth = Mathf.Clamp(maxValue, 0f, maxValue);
+
+        rectTransform.DOSizeDelta(new Vector2(targetWidth, rectTransform.sizeDelta.y), statusValueTime)
+           .SetEase(easeType);
+
+        rectTransform_1.DOSizeDelta(new Vector2(targetWidth, rectTransform_1.sizeDelta.y), statusValueTime)
+           .SetEase(easeType);
+        
+    }
+
+    public void DecreasemMax(float amount)
+    {
+        //更新最大上限
+        maxValue = maxValue - amount;
+
+        RectTransform rectTransform = statusValueimage.GetComponent<RectTransform>();
+
+        float targetWidth = Mathf.Clamp(maxValue, 0f, maxValue);
+
+        rectTransform.DOSizeDelta(new Vector2(targetWidth, rectTransform.sizeDelta.y), statusValueTime)
+           .SetEase(easeType);
+       
+    }
+
+    public void DecreseValueToMax(float amount)
+    {
+        RectTransform rectTransform = backGround.GetComponent<RectTransform>();
+        RectTransform rectTransform_1 = statusValueBack.GetComponent<RectTransform>();
+        RectTransform rectTransform_2 = statusValueimage.GetComponent<RectTransform>();
+
+        float targetWidth = Mathf.Clamp(maxValue, 0f, maxValue);
+
+        rectTransform.DOSizeDelta(new Vector2(targetWidth + 10, rectTransform.sizeDelta.y), statusValueTime)
+           .SetEase(easeType);
+
+        rectTransform_1.DOSizeDelta(new Vector2(targetWidth, rectTransform_1.sizeDelta.y), statusValueTime)
+           .SetEase(easeType);
+
     }
 
 
@@ -113,6 +206,22 @@ public class Statusvaluecontroller : MonoBehaviour
 
         ChangeValue_step();
 
+    }
+
+    public IEnumerator IncreseMax(float amount)
+    {
+        IncreasemMax(amount);
+        yield return new WaitForSeconds(0.1f);
+
+        IncreseValueToMax(amount);
+    }
+
+    public IEnumerator DecreseMax(float amount)
+    {
+        DecreasemMax(amount);
+        yield return new WaitForSeconds(0.1f);
+
+        DecreseValueToMax(amount);
     }
     #endregion
 
